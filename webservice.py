8f3533b9296df2f5cdc64f44146568a1c2065e49
python240419 <51232248+python240419@users.noreply.github.com>
2019-11-22 11:26:03 +0200

    Update webservice.py

--- webservice.py
+++ webservice.py
@@ -12,19 +12,24 @@ def home_page():
 
 @app.route('/messages', methods=['GET', 'POST','PUT', 'DELETE'])
 def webservice():
-    messages = { 'messages' :
-                       [{'id' : 1, 'title' : 'Hello'},
-                         {'id' : 2, 'title' : 'How are you?'}] }
-    #print(request.data)
-    #d = json.loads(request.data)
-    #print(d)
+    global messages
     if request.method == 'GET':
         return messages
     if request.method == 'POST':
+        print(request.data)
+        d = json.loads(request.data)
+        print(d)
+        # todo - add the d item to messages
         return 'You posted'
     if request.method == 'PUT':
+        # todo - the same but update
         return 'You put -- update'
     if request.method == 'DELETE':
+        # todo - delete
         return 'You deleted'
+    
+messages = { 'messages' :
+    [{'id' : 1, 'title' : 'Hello'},
+    {'id' : 2, 'title' : 'How are you?'}] }
         
 app.run(debug=True)
